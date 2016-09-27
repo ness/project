ad8c84b522993293ef1e2a2e86275ab656ff4958
ness <nessuno@gmx.de>
2016-09-27 09:18:02 +0200

    Add cdp and __ps1 helpers

--- bash/project.sh
+++ bash/project.sh
@@ -6,3 +6,21 @@ project_activate () {
 		. $project_dir/.project/bin/activate
 	fi
 }
+
+cdp () {
+	cd $(dirname $PROJECT_DIR)
+}
+
+__ps1_project () {
+	$(__ps1_is_project)
+	if [ $? ]; then
+		echo $(basename $(dirname $PROJECT_DIR))
+	fi
+}
+
+__ps1_is_project () {
+	if [ -n "${PROJECT_DIR+set}" ]; then
+		exit 0
+	fi
+	exit 1
+}
