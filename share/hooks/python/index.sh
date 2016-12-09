31b3ec603f7c3cba60d75834f735c5d9d3b2aad2
ness <nessuno@gmx.de>
2016-12-09 09:28:38 +0100

    Use source_dirs from config to generate tag files

--- share/hooks/python/index.sh
+++ share/hooks/python/index.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ctags -R --fields=+l --languages=python --python-kinds=-iv -f $PROJECT_DIR/.tags .
+old_IFS=$IFS
+IFS=$'\n'
+
+SOURCE_DIRS=( $(project_configuration iter --key source_dirs --format newline ) )
+# LIB_DIRS=( $(project_configuration show --key lib_dirs --format newline ) )
+
+IFS=$old_IFS
+
+
+ctags -R --fields=+l --languages=python --python-kinds=-iv -f $PROJECT_DIR/.tags.sources "${SOURCE_DIRS[@]}"
+ctags -R --fields=+l --languages=python --python-kinds=-iv -f $PROJECT_DIR/.tags.lib "${LIB_DIRS[@]}"
